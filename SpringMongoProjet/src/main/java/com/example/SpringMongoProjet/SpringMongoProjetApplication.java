fa9c7183308c2f2a531c3ee4dc469e83da9bc226
fsalle1954 <fsalle2002@gmail.com>
2024-07-14 16:30:00 +0200

    SpringMongoProjet

---
+++ SpringMongoProjet/src/main/java/com/example/SpringMongoProjet/SpringMongoProjetApplication.java
@@ -0,0 +1,13 @@
+package com.example.SpringMongoProjet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMongoProjetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMongoProjetApplication.class, args);
+	}
+
+}
---
+++ SpringMongoProjet/src/test/java/com/example/SpringMongoProjet/SpringMongoProjetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringMongoProjet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMongoProjetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
